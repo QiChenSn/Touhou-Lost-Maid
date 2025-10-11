2c4b016d07146f73954361b181aa9cc3e894936f
QiChen <2102733232@qq.com>
2025-10-11 20:59:38 +0800

    feat: 实现击杀驯服女仆

---
+++ src/main/java/com/github/qichensn/event/ModMaidDeathEvent.java
@@ -0,0 +1,27 @@
+package com.github.qichensn.event;
+
+import com.github.qichensn.TouhouLostMaid;
+import com.github.qichensn.data.LostMaidData;
+import com.github.tartaricacid.touhoulittlemaid.api.LittleMaidExtension;
+import com.github.tartaricacid.touhoulittlemaid.api.event.MaidDeathEvent;
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+
+@EventBusSubscriber(modid = TouhouLostMaid.MODID)
+public class ModMaidDeathEvent {
+    @SubscribeEvent
+    public static void onDeath(MaidDeathEvent  event) {
+        EntityMaid maid = event.getMaid();
+        DamageSource source = event.getSource();
+
+        // 玩家击杀后驯服
+        if(maid.getOrCreateData(LostMaidData.IS_LOST_MAID,false) && source.getEntity() instanceof Player player) {
+            maid.setOwnerUUID(player.getUUID());
+            maid.setData(LostMaidData.IS_LOST_MAID,true);
+        }
+    }
+}
