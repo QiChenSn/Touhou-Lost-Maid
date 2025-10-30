dd0000e0e3a43e239e3f6afaf43042f5b4022d19
QiChen <2102733232@qq.com>
2025-10-30 13:46:51 +0800

    fix: 修复了未安装卓越前线导致的崩溃

--- src/main/java/com/github/qichensn/compat/ExtractModCheck.java
+++ src/main/java/com/github/qichensn/compat/ExtractModCheck.java
@@ -3,8 +3,9 @@ package com.github.qichensn.compat;
 import com.github.tartaricacid.touhoulittlemaid.compat.gun.swarfare.SWarfareCompat;
 
 public class ExtractModCheck {
+    // 注意: 此方法只有在FML后调用才是准确的
+    // 如果在FML阶段应该先尝试调用init(), 再进行判断
     public static boolean isSWarfareLoaded(){
         return SWarfareCompat.isInstalled();
     }
-
 }
--- src/main/java/com/github/qichensn/util/ItemUtil.java
+++ src/main/java/com/github/qichensn/util/ItemUtil.java
@@ -2,19 +2,32 @@ package com.github.qichensn.util;
 
 import com.atsuishio.superbwarfare.init.ModItems;
 import com.github.qichensn.data.LostMaidData;
+import com.github.tartaricacid.touhoulittlemaid.compat.gun.swarfare.SWarfareCompat;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.items.wrapper.CombinedInvWrapper;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class ItemUtil {
-    // 检查并且删除女仆背包中的违禁物品
-    // TODO:可配置
-    public static Set<Item> BANNED_ITEMS = Set.of(ModItems.CREATIVE_AMMO_BOX.get());
+import static com.github.qichensn.compat.ExtractModCheck.isSWarfareLoaded;
 
+public class ItemUtil {
+
+    // TODO:可配置
+    public static Set<Item> BANNED_ITEMS = new HashSet<>();
+
+    static {
+        // 先检查是否安装了卓越前线
+        // 此方法应该在游戏内调用, 此时isInstalled返回的结果是准确的
+        if(isSWarfareLoaded()){
+            BANNED_ITEMS.add(ModItems.CREATIVE_AMMO_BOX.get());
+        }
+    }
+
+    // 检查并且删除女仆背包中的违禁物品
     public static void deleteBannedItems(EntityMaid maid) {
         // 只处理迷失女仆
         if (!maid.getOrCreateData(LostMaidData.IS_LOST_MAID, false)) {
