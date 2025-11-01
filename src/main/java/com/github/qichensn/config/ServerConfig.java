ad43c26ea9d4876ace9e5c40cf8f128bc761f7bd
QiChen <2102733232@qq.com>
2025-11-01 16:40:35 +0800

    feat: 注册可驯服女仆数量配置

--- src/main/java/com/github/qichensn/config/ServerConfig.java
+++ src/main/java/com/github/qichensn/config/ServerConfig.java
@@ -32,38 +32,44 @@ public class ServerConfig {
     // 女仆装备黑名单配置
     public static ModConfigSpec.ConfigValue<List<String>> EQUIPMENT_BANNED_ITEMS;
 
+    // 可驯服迷失女仆数量
+    public static ModConfigSpec.IntValue CAN_TAME_COUNT;
+
     public static ModConfigSpec init(){
         ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
         builder.comment("女仆服务器配置");
 
         builder.comment("出生点附近禁止生成女仆的半径");
-        SPAWN_PROTECT_RANGE = builder.defineInRange("Spawn_Protect_Range", 1000, 0, Integer.MAX_VALUE);
+        SPAWN_PROTECT_RANGE = builder.defineInRange("spawn_protect_range", 1000, 0, Integer.MAX_VALUE);
 
         builder.comment("近战女仆生成权重");
-        ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("Attack_Lost_Maid_Spawn_Weight",100,1,Integer.MAX_VALUE);
+        ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("attack_lost_maid_spawn_weight",100,1,Integer.MAX_VALUE);
 
         builder.comment("弓箭远程女仆生成权重");
-        BOW_ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("Bow_Attack_Lost_Maid_Spawn_Weight",30,0,
+        BOW_ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("bow_attack_lost_maid_spawn_weight",30,0,
                 Integer.MAX_VALUE);
 
         builder.comment("枪械女仆生成权重");
-        GUN_ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("Gun_Attack_Lost_Maid_Spawn_Weight",10,0,
+        GUN_ATTACK_LOST_MAID_SPAWN_WEIGHT=builder.defineInRange("gun_attack_lost_maid_spawn_weight",10,0,
                 Integer.MAX_VALUE);
 
         builder.comment("弓箭远程女仆使用药水箭的概率");
-        TIPPED_ARROW_CHANCE=builder.defineInRange("Tipped_Arrow_Chance",0.3,0,1);
+        TIPPED_ARROW_CHANCE=builder.defineInRange("tipped_arrow_chance",0.3,0,1);
 
         builder.comment("女仆射出的药水箭矢为正面效果的概率");
         builder.comment("说明: 正面效果可对玩家(刻意为之)和女仆生效, 负面效果只对玩家生效");
-        BENEFIT_ARROW_CHANCE=builder.defineInRange("Benefit_Arrow_Chance",0.2,0,1);
+        BENEFIT_ARROW_CHANCE=builder.defineInRange("benefit_arrow_chance",0.2,0,1);
 
         builder.comment("女仆掉落物黑名单配置");
         builder.comment("说明: 填入物品的注册名，例如: [\"minecraft:diamond\", \"minecraft:enchanted_golden_apple\"]");
-        BANNED_ITEMS = builder.define("Banned_Items", Lists.newArrayList());
+        BANNED_ITEMS = builder.define("banned_items", Lists.newArrayList());
 
         builder.comment("女仆装备黑名单配置");
-        EQUIPMENT_BANNED_ITEMS = builder.define("Equipment_Banned_Items", Lists.newArrayList());
+        EQUIPMENT_BANNED_ITEMS = builder.define("equipment_banned_items", Lists.newArrayList());
+
+        builder.comment("可以通过驯服的迷失女仆的最大数量");
+        CAN_TAME_COUNT = builder.defineInRange("can_tame_count",5,0,Integer.MAX_VALUE);
 
         return builder.build();
     }
